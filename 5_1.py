11c9d692fb32b76dc253cd4bdfa4966ba1a4f8f1
Еприцкий_Денис_8И22 <dae29@tpu.ru>
2023-07-10 11:59:42 +0800

    innit

---
+++ 5_1.py
@@ -0,0 +1,9 @@
+import csv
+
+with open("5_1.csv", mode="w", encoding="utf-8")as w_file:
+    file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+    file_writer.writerow(["Книга", "Автор", "Год выпуска"])
+    file_writer.writerow(["Страж", "Алексей Пехов", "2010"])
+    file_writer.writerow(["Красное на красном", "Вера Камша", "2004"])
+    file_writer.writerow(["Человек, который принял жену за шляпу", "Оливер Сакс", "2021"])
+    file_writer.writerow(["Убийственная шутка", "Криста Фауст", "2019"])
