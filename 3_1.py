c524bca9436a4cbde9946b0781e85766d1e75667
Еприцкий_Денис_8И22 <dae29@tpu.ru>
2023-07-18 12:31:09 +0800

    fix the pycodestyle mistakes

--- 3_1.py
+++ 3_1.py
@@ -4,5 +4,5 @@ from functools import reduce
 l = input().split()
 a = []
 for i in range(0, len(l)):
-   a.append(float(l[i]))
+    a.append(float(l[i]))
 print(reduce(lambda x, y: x + y, a) / len(l))
